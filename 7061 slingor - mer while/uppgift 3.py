5dc989937f29e36b44bfc913a381a6b36a2b398d
Hugo Myrberg <hugo.myrberg05@skola.stenungsund.se>
2023-09-14 13:53:41 +0200

    fixat

--- 7061 slingor - mer while/uppgift 3.py
+++ 7061 slingor - mer while/uppgift 3.py
@@ -1,6 +1,9 @@
 svar = int(input("skriv ett tal"))
 antal_gissningar = 1
 
+# igen om antal gissningar < 3
+# igen om gissat fel o
+# sluta om gissar rätt
 
 while svar != 42 and antal_gissningar <3:
     if svar < 42:
@@ -10,7 +13,8 @@ while svar != 42 and antal_gissningar <3:
     if svar > 42:
         print("för stort. ")
         svar = int(input("gissa igen: "))
+
     antal_gissningar = antal_gissningar + 1
 if svar == 42:
     print("du är bäst")
-    print(antal_gissningar, "gisningar behövde du")
+print(antal_gissningar, "är tyvärr det maximala antal gissningar du får på dig. Vill du köra igen så starta om programmet")
