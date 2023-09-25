f9456001f3ecbca29944859614bcf77aa650ef7b
Hugo Myrberg <hugo.myrberg05@skola.stenungsund.se>
2023-09-25 13:22:29 +0200

    py

---
+++ 7070/random.py
@@ -0,0 +1,11 @@
+import random
+i = 0
+while i < 5:
+    t1 = random.randrange(1,7)
+    t2 = random.randrange(1,7) 
+    print("tärning 1:", t1)
+    print("tärning 2:", t2)
+    summa = t1 + t2
+    print("Summan av tärningskasten är:", summa)
+    i = i + 1
+
