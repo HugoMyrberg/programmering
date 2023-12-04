8d00f8db07e83166462667177dcac917f4023440
Hugo Myrberg <hugo.myrberg05@skola.stenungsund.se>
2023-12-04 12:47:50 +0100

    hej

--- 7100 listor/hej.py
+++ 7100 listor/hej.py
@@ -1,6 +1,19 @@
-x = [1, 0, 1, 1, 0]
-for y in x:
-    if y == 1:
-        print ("hej")
-    else:
-        print(y)
+def a():
+ print("b")
+def c():
+ return("d")
+def e(f):
+ return f * f
+def i(j):
+ print("a", j)
+def l(m):
+ return"m" + m
+def o(p):
+ return p + 2
+a()
+g = c()
+print(g)
+print(e(3))
+i("c")
+print(l("o"))
+print(o(4))
